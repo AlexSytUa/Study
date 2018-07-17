abd777f99525168e0e9fcef491999d57b715f407
Alexey <alex.syt.ua@gmail.com>
2018-07-17 21:59:48 +0300

    Fix: With new settings for sublime

--- Lesson1/quadratic_equation.rb
+++ Lesson1/quadratic_equation.rb
@@ -9,12 +9,12 @@ d = side_b**2 - 4 * side_a * side_c
 d_sqrt = Math.sqrt( d ) 
 
 if (d > 0)
-	х1 = (-side_b + d_sqrt) / (2 * side_a)
-	x2 = (-side_b - d_sqrt) / (2 * side_a)
-	puts "D = #{d}; x1=#{х1}; x2=#{x2}"
-elsif(d == 0)
-	х1 = -side_b / (2 * side_a)
-	puts "D = #{d}; x1=#{х1}"
+  х1 = (-side_b + d_sqrt) / (2 * side_a)
+  x2 = (-side_b - d_sqrt) / (2 * side_a)
+  puts "D = #{d}; x1=#{х1}; x2=#{x2}"
+elsif (d == 0)
+  х1 = -side_b / (2 * side_a)
+  puts "D = #{d}; x1=#{х1}"
 else
-	puts "D = #{d}; Корней нет"
+  puts "D = #{d}; Корней нет"
 end
--- Lesson1/right_triangle.rb
+++ Lesson1/right_triangle.rb
@@ -7,7 +7,7 @@ side_c = gets.to_f
 
 side_array = [side_a,side_b,side_c].sort
 
-if(side_array.uniq.size > 1)
+if (side_array.uniq.size > 1)
   if (side_array[2] == Math.sqrt(side_array[0]**2 + side_array[1]**2))
     puts "Треугольник прямоугольный"
     if (side_array[0] == side_array[1])
