9d44efc5baa7afe1cb434fa5c5a670bbc32766d9
Alexey <alex.syt.ua@gmail.com>
2018-07-18 21:58:29 +0300

    Fix: New comments

--- Lesson1/quadratic_equation.rb
+++ Lesson1/quadratic_equation.rb
@@ -6,9 +6,10 @@ puts "Введите сторону C"
 side_c = gets.to_f
 
 d = side_b**2 - 4 * side_a * side_c
-d_sqrt = Math.sqrt( d ) 
+
 
 if (d > 0)
+  d_sqrt = Math.sqrt(d)
   х1 = (-side_b + d_sqrt) / (2 * side_a)
   x2 = (-side_b - d_sqrt) / (2 * side_a)
   puts "D = #{d}; x1=#{х1}; x2=#{x2}"
--- Lesson1/right_triangle.rb
+++ Lesson1/right_triangle.rb
@@ -5,14 +5,13 @@ side_b = gets.to_f
 puts "Введите сторону C треугольника"
 side_c = gets.to_f
 
-side_array = [side_a,side_b,side_c].sort
+side_array = [side_a, side_b, side_c].sort
 
-if (side_array.uniq.size > 1)
-  if (side_array[2] == Math.sqrt(side_array[0]**2 + side_array[1]**2))
-    puts "Треугольник прямоугольный"
-    if (side_array[0] == side_array[1])
-  	  puts "Треугольник равнобедренный"
-    end
-  end
+is_triangle = side_array[2] == Math.sqrt(side_array[0]**2 + side_array[1]**2)
+is_rectangular = side_array.uniq.size > 1
+is_isosceles = side_array[0] == side_array[1]
+
+if(is_triangle && is_rectangular)
+  puts "Треугольник прямоугольный"
+ puts "Треугольник равнобедренный" if is_isosceles 
 end
-
